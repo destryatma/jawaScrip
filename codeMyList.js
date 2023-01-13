dda05599b2e557712432cca34d33635ce050cfb3
Tryatma <tryatma.sa@gmail.com>
2023-01-13 15:32:06 +0700

    baru bisa nambah

--- codeMyList.js
+++ codeMyList.js
@@ -1,14 +1,23 @@
-function Daftar(){
-  let textList = document.getElementsByTagName('input')[0].value;
+let i = 0
+function daftar(){
+  const textList = document.getElementsByTagName('input')[0].value;
   const tempatList = document.getElementsByTagName("a")[0];
-  tempatList.insertAdjacentText("beforeend", textList);
+  const barisLi = document.createElement('li');
+  const deleteBtn = document.createElement('button');
+  const killBtn = document.createAttribute("onclick");
+  
 
+  killBtn.value = 'deleteX()';
+  deleteBtn.innerText = 'X';
+  barisLi.innerText = textList;
 
-//  const textNode = document.createTextNode(textList);
-//  const barisLi = document.createElement('li');
-  // barisLi.textContent = textList;
-  // tempatList.insertAdjacentText('afterbegin', barisLi);
   
-  
-  
-  }
+  tempatList.insertAdjacentElement("afterend", barisLi);
+  barisLi.insertAdjacentElement("beforeend",deleteBtn);
+  deleteBtn.setAttributeNode(killBtn);
+}
+
+function deleteX() {
+  let nmrList = document.getElementsByTagName("li");
+  nmrList.remove
+}
